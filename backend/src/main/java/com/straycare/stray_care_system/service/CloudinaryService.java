aa127d6b4e0d204fb10826289e9e30c0bbeaa8d6
Paucey <alexanderhagen@live.com>
2026-04-21 12:18:36 -0300

    Fix problem with Cloudinary constructor

--- backend/src/main/java/com/straycare/stray_care_system/service/CloudinaryService.java
+++ backend/src/main/java/com/straycare/stray_care_system/service/CloudinaryService.java
@@ -6,6 +6,7 @@ import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -21,6 +22,7 @@ public class CloudinaryService {
     private final Cloudinary cloudinary;
     private final String placeholderUrl;
 
+    @Autowired
     public CloudinaryService(
             @Value("${cloudinary.cloud-name}") String cloudName,
             @Value("${cloudinary.api-key}") String apiKey,
